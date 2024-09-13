29c67e6dfa29a1bbcae42234d89d2cbf36d1869f
Teju <tejumoluwaogunleye@gmail.com>
2024-09-13 14:31:27 +0100

    This is the first commit after adding localization

---
+++ LocalizedDate.java
@@ -0,0 +1,8 @@
+public class Sample{
+	public static void main(String[] args){
+		System.out.println("This is just a sample java file for Git");
+		System.out.println("We are adding the output lines to see the changes.");
+		System.out.println("This is 2nd modification in java file to see the staged and unstaged 
+			changes.");
+}
+}
